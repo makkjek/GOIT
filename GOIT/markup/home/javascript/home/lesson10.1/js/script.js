3f28788f186d992b7e7166c5eb3238d28f4905f2
Lana Kyrylyuk <makkjek@gmail.com>
2015-12-25 17:41:08 +0200

    new files added

--- GOIT/markup/home/javascript/home/lesson10.1/js/script.js
+++ GOIT/markup/home/javascript/home/lesson10.1/js/script.js
@@ -1,16 +1,19 @@
 function enterNum (n1, n2) {
-    result = 1;
-    for (var i = 0; i < n2; i++) {
-        result = result * n1;
-        console.log ("Число" + n1 + "в степени" +(i + 1) + "=" + result);
+    result = n1;
+    var isDegreeNegative = false ;
+    if (n2 < 0) {
+        n2 *= -1;
+        isDegreeNegative = true;
     }
-    return result;
+    for (var i = 1; i < n2; i++) {
+        result *= n1;
+    }
+    if (isDegreeNegative) {
+        return 1/result;
+    } else return result;
+
 }
-var n1 = parseInt (prompt ('Input first number'));
-var n2 = parseInt (prompt ('Input the second number'));
-console.log ("result:" + Math. pow (n1, n2));
-if (n1 > 0) {
-    console.log ("result of functin:" + enterNum (n1, n2));
-} else {
-    alert ("Input number bigger than 0");
-}
+var number = parseInt (prompt ('Input first number'));
+var degree = parseInt (prompt ('Input the second number'));
+console.log ("result:" + Math. pow (number, degree));
+alert ("result of functin:" + enterNum (number, degree));
